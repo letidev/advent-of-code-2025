c19a01b34b6cffac99a781d4f5134ecb6168bb35
Violeta Malinova <violeta@mlnova.com>
2025-12-02 13:57:43 +0200

    1 Dec Part 2

--- 1dec/solution.py
+++ 1dec/solution.py
@@ -3,24 +3,32 @@ with open("test.txt") as f:
 
 dial = 50
 zeros = 0
+at0 = False  # shows whether you start the current rotation from 0
 
 for rotation in rotations:
-    direction = rotation[:1]
-    amount = int(rotation[1:]) % 100
+    direction, amount = rotation[:1], int(rotation[1:])
+    at0 = dial == 0
 
     if direction == "L":
-        dial -= amount
+        dial = dial - amount
 
-        if dial < 0:
-            dial = 100 + dial
+        if dial <= 0:
+            zeros += abs(dial) // 100
+
+            if not at0:
+                zeros += 1
+
+            if dial % 100 == 0:
+                dial = 0
+            elif dial != 0:
+                dial = 100 - (abs(dial) % 100)
 
     if direction == "R":
         dial += amount
 
         if dial >= 100:
+            zeros += dial // 100
             dial = dial % 100
 
-    if dial == 0:
-        zeros += 1
 
 print(zeros)
