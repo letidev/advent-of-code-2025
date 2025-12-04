7644fb20026eab9a17d70445b2a738af325a2d5e
Violeta Malinova <violeta@mlnova.com>
2025-12-04 09:03:01 +0200

    4 Dec Part 2

--- 4dec/solution.py
+++ 4dec/solution.py
@@ -1,23 +1,34 @@
 with open("test.txt") as f:
     lines = f.read().splitlines()
 
-rolls = 0
+total_rolls = 0
 
 directions = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 
-for i in range(len(lines)):
-    for j in range(len(lines[i])):
+removed = [[False for _ in range(len(lines[0]))] for _ in range(len(lines))]
+rolls = 1
 
-        if lines[i][j] == "@":
-            adj = 0
-            for d in directions:
-                ai, aj = i + d[0], j + d[1]
+while rolls != 0:
 
-                if ai >= 0 and ai < len(lines) and aj >= 0 and aj < len(lines[i]):
-                    if lines[ai][aj] == "@":
-                        adj += 1
+    rolls = 0
 
-            if adj < 4:
-                rolls += 1
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
 
-print(rolls)
+            if lines[i][j] == "@" and not removed[i][j]:
+                adj = 0
+                for d in directions:
+                    ai, aj = i + d[0], j + d[1]
+
+                    if ai >= 0 and ai < len(lines) and aj >= 0 and aj < len(lines[i]):
+                        if lines[ai][aj] == "@" and not removed[ai][aj]:
+                            adj += 1
+
+                if adj < 4:
+                    removed[i][j] = True
+                    rolls += 1
+
+    if rolls > 0:
+        total_rolls += rolls
+
+print(total_rolls)
