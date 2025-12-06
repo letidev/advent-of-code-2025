3d51b6e613f978468b57fba09bb1391876ced8b3
Violeta Malinova <violeta@mlnova.com>
2025-12-06 09:39:10 +0200

    6 Dec Part 2

--- 6dec/solution.py
+++ 6dec/solution.py
@@ -22,4 +22,38 @@ for i in range(1, len(lines) - 1):
         else:
             nums[j] *= nums2[j]
 
-print(sum(nums))
+print("Part 1:", sum(nums))
+
+# Part 2
+total = 0
+
+# get real indices of signs
+idx = []
+i = -1
+for s in signs:
+    i = lines[-1].find(s, i + 1)
+    idx.append(i)
+    i += 1
+
+for i in range(len(idx)):
+    start = idx[i]
+    end = idx[i + 1] - 1 if i + 1 < len(idx) else len(lines[0])
+
+    res = 0
+    if signs[i] == "*":
+        res = 1
+
+    for j in range(start, end):
+        num = 0
+        for k in range(0, len(lines) - 1):
+            if lines[k][j] != " ":
+                num = num * 10 + int(lines[k][j])
+
+        if signs[i] == "+":
+            res += num
+        else:
+            res *= num
+
+    total += res
+
+print("Part 2:", total)
