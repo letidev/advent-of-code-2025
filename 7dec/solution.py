90ffe406d9ffd728ea221ce6b76e8a41a1904455
Violeta Malinova <violeta@mlnova.com>
2025-12-08 09:42:09 +0200

    7 Dec Part 2

--- 7dec/solution.py
+++ 7dec/solution.py
@@ -7,6 +7,7 @@ x, y = 0, grid[0].find("S")
 visited = [[False for _ in range(m)] for _ in range(n)]
 
 
+# Part 1
 def beams(x, y, n, m):
     if x < 0 or x >= n or y < 0 or y >= m or visited[x][y]:
         return 0
@@ -20,3 +21,27 @@ def beams(x, y, n, m):
 
 
 print(beams(x, y, n, m))
+
+# Part 2
+found = [[-1 for _ in range(m)] for _ in range(n)]
+
+
+def paths(x, y, n, m):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return 0
+
+    if x == n - 1:
+        return 1
+
+    if found[x][y] != -1:
+        return found[x][y]
+
+    if grid[x][y] == "^":
+        found[x][y] = paths(x + 1, y - 1, n, m) + paths(x + 1, y + 1, n, m)
+    else:
+        found[x][y] = paths(x + 1, y, n, m)
+
+    return found[x][y]
+
+
+print(paths(x, y, n, m))
